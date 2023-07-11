b9739ac32caaea6a823de3887d6abfb93c0dc46a
ebtrader <jsiddique@gmail.com>
2023-07-11 17:33:50 -0400

    Commit

--- monitor_update.sh
+++ monitor_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the Python script is running
-if ! pgrep -f "$HOME/env/bin/python3 /home/ubuntu/market_depth_gateway.py" > /dev/null; then
+if ! pgrep -f "~/env/bin/python3 /home/ubuntu/market_depth_gateway.py" > /dev/null; then
     echo "Python script is not running. Restarting..."
     ~/env/bin/python3 /home/ubuntu/market_depth_gateway.py >/dev/null 2>&1 &
 else
