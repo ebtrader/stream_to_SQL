33d5a2da6c2af1aa433e652021d7882a068b6bf7
ebtrader <jsiddique@gmail.com>
2023-07-12 15:24:32 -0400

    Commit

--- monitor_update.sh
+++ monitor_update.sh
@@ -7,3 +7,18 @@ if ! pgrep -f "~/env/bin/python3 /home/ubuntu/market_depth_gateway.py" > /dev/nu
 else
     echo "Python script is running."
 fi
+
+try this instead
+
+#!/bin/bash
+
+. /etc/profile
+export PATH=$PATH:/usr/bin
+
+# Check if the Python script is running
+if ! pgrep -f "~/env/bin/python3 /home/ubuntu/streaming_ec2.py" > /dev/null; then
+        echo "Python script is not running. Restarting..."
+        ~/env/bin/python3 /home/ubuntu/streaming_ec2.py >/dev/null 2>&1 &
+else
+        echo "Python script is running."
+fi
