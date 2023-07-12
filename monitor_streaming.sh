1402ce18947995902fa95fc8344a39c0eba74928
ebtrader <jsiddique@gmail.com>
2023-07-12 15:23:41 -0400

    Commit

--- monitor_streaming.sh
+++ monitor_streaming.sh
@@ -7,3 +7,19 @@ if ! pgrep -f "~/env/bin/python3 /home/ubuntu/streaming_ec2.py" > /dev/null; the
 else
     echo "Python script is running."
 fi
+
+
+# try this one it worked
+
+#!/bin/bash
+
+. /etc/profile
+export PATH=$PATH:/usr/bin
+
+# Check if the Python script is running
+if ! pgrep -f "~/env/bin/python3 /home/ubuntu/streaming_ec2.py" > /dev/null; then
+        echo "Python script is not running. Restarting..."
+        ~/env/bin/python3 /home/ubuntu/streaming_ec2.py >/dev/null 2>&1 &
+else
+        echo "Python script is running."
+fi
