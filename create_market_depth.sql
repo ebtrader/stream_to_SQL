8a9b6c45e2bdfd976c7cf67182180a82f6d2eed0
ebtrader <jsiddique@gmail.com>
2023-07-16 21:19:08 -0400

    Commit

--- create_market_depth.sql
+++ create_market_depth.sql
@@ -8,4 +8,4 @@ CREATE TABLE `updatemarketdepth` (
   `SIZE` int NOT NULL,
   `timestamp` varchar(100) NOT NULL,
   UNIQUE KEY `id_unique` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=135394 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
